5b62cb273e9910550f73b835bb0d606ef5b97ddb
FailedXAssassin <hughesjohn323@aol.com>
2026-09-14 19:34:55 -0400

    Refresh cached app after startup compatibility fix

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE = "tcg-radar-v3-5";
+const CACHE = "tcg-radar-v3-6";
 const ASSETS = ["./","index.html","style.css","app.js","manifest.webmanifest","icon-radar-v2.svg"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", e => e.waitUntil(Promise.all([
