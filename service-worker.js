bd50808e8df7a97e00496c95ebf1714caaacb80d
FailedXAssassin <hughesjohn323@aol.com>
2026-09-14 02:29:13 -0400

    Use current radar icon for push notifications

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE = "tcg-radar-v3-4";
+const CACHE = "tcg-radar-v3-5";
 const ASSETS = ["./","index.html","style.css","app.js","manifest.webmanifest","icon-radar-v2.svg"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", e => e.waitUntil(Promise.all([
@@ -12,6 +12,7 @@ self.addEventListener("fetch", e => {
 
 self.addEventListener("push", event => {
   const data = event.data ? event.data.json() : {};
-  event.waitUntil(self.registration.showNotification(data.title || "TCG Radar", {body:data.body || "A product changed status.", icon:"./icon.svg", badge:"./icon.svg", tag:data.tag, data:{url:data.url || "./"}}));
+  event.waitUntil(self.registration.showNotification(data.title || "TCG Radar", {body:data.body || "A product changed status.", icon:"./icon-radar-v2.svg", badge:"./icon-radar-v2.svg", tag:data.tag, data:{url:data.url || "./"}}));
 });
 self.addEventListener("notificationclick", event => { event.notification.close(); event.waitUntil(clients.openWindow(event.notification.data.url)); });
+
