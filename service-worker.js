39eae9683c0fd069d8f75bfc2485a67e76f068d3
FailedXAssassin <hughesjohn323@aol.com>
2026-09-14 00:58:36 -0400

    Bust Android icon cache for new radar logo

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 
 const CACHE = "tcg-radar-v3-4";
-const ASSETS = ["./","index.html","style.css","app.js","manifest.webmanifest","icon.svg"];
+const ASSETS = ["./","index.html","style.css","app.js","manifest.webmanifest","icon-radar-v2.svg"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", e => e.waitUntil(Promise.all([
   caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))),
